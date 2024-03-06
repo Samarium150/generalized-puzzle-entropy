7e0c6e5afe6bbc18c2f431158eea851357033030
Samarium <28302241+Samarium150@users.noreply.github.com>
2024-03-06 03:09:51 -0700

    fix: correct prepare.ps1
    (cherry picked from commit 5a227f63a9e05f1fb4065d25b68bacf392bac235)

--- packages/puzzle-editor/scripts/prepare.ps1
+++ packages/puzzle-editor/scripts/prepare.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = "Stop"
 Set-Location -Path "$(git rev-parse --show-toplevel)\packages\puzzle-editor"
 git submodule update --init --recursive
 python -m venv .venv
+.\.venv\Scripts\python.exe -m pip install -r requirements.txt --upgrade pip
 Set-Location -Path "lib\emsdk"
 .\emsdk.ps1 install latest
 .\emsdk.ps1 activate latest
