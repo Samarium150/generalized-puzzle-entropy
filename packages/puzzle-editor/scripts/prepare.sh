eac7a5135abb3259134fbba14ff23a328b22e7d0
Samarium150 <28302241+Samarium150@users.noreply.github.com>
2024-06-10 16:43:40 -0600

    build: bump dependencies

--- packages/puzzle-editor/scripts/prepare.sh
+++ packages/puzzle-editor/scripts/prepare.sh
@@ -1,6 +1,6 @@
 set -e
 cd "$(git rev-parse --show-toplevel)/packages/puzzle-editor"
-git submodule update -f --depth 1 --recommend-shallow
+git submodule update --checkout -f --depth 1 --recommend-shallow
 python3 -m venv .venv
 ./.venv/bin/python3 -m pip install -r requirements.txt
 cd lib/emsdk
